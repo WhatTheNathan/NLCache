227bd18bed27b02940d02be6d275ae4d500df835
WhatTheNathan <nathanliuyolo@gmail.com>
2018-05-13 16:52:00 +0800

    initial commit

--- NLCache/NLMemoryCache.swift
+++ NLCache/NLMemoryCache.swift
@@ -7,3 +7,36 @@
 //
 
 import Foundation
+
+/**
+ * NLMemoryCache is a fast in-memory cache that stores key-value pairs
+ **/
+open class NLMemoryCache {
+    
+    // shared instance
+    static let shared = NLMemoryCache()
+    
+    //
+//    fileprivate queue = 
+    
+    // Lock
+    fileprivate let semaphoreLock = DispatchSemaphore(value: 1)
+    
+    /* Designed constructer */
+    private init() {
+        
+    }
+}
+
+/**
+ * Lock for thread-safe
+ **/
+extension NLMemoryCache {
+    fileprivate func lock() {
+        _ = semaphoreLock.wait(timeout: .distantFuture)
+    }
+    
+    fileprivate func unlock() {
+        semaphoreLock.signal()
+    }
+}
